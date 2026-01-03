a470618faa1b5c0e514b39a77dc5ecd744408c1c
CertificationTeacher <gitub@cert2day.com>
2026-01-03 17:10:50 +0500

    Finalize Cloudinary image storage

--- TeacherApp/models.py
+++ TeacherApp/models.py
@@ -1,41 +1,45 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
 class herotext(models.Model):
     toptext = models.CharField(max_length=30)
     hero_title = models.CharField(max_length=200)
-    hero_description= models.CharField(max_length=300)
+    hero_description = models.CharField(max_length=300)
     is_active = models.BooleanField(default=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.toptext
 
 
 class contentcolumn(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = CloudinaryField("image", blank = True, null = True)
+    image = CloudinaryField("image", blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
+
 class Certification(models.Model):
     description = models.TextField()
-    image = CloudinaryField("image", blank = True, null = True)
+    image = CloudinaryField("image", blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.description
+
+
 class contentbody(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = CloudinaryField("image", blank = True, null = True)
+    image = CloudinaryField("image", blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=150)
@@ -46,27 +50,33 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return self.name
+
+
 class footer(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = CloudinaryField("image", blank = True, null = True)
+    image = CloudinaryField("image", blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
-    
+
+
 class comparison(models.Model):
     labels = models.CharField(max_length=200)
     teacher = models.TextField()
     others = models.TextField()
-    is_active=models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
     def __str__(self):
         return self.labels
+
 
 class whyus(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = CloudinaryField("image", blank = True, null = True)
+    image = CloudinaryField("image", blank=True, null=True)
     is_active = models.BooleanField(default=True)
+
     def __str__(self):
         return self.title

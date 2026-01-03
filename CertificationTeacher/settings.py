7819875d04467fcc95285c63c2571eeaf48bdf2b
CertificationTeacher <gitub@cert2day.com>
2026-01-03 16:39:25 +0500

    Update settings.py

--- CertificationTeacher/settings.py
+++ CertificationTeacher/settings.py
@@ -38,7 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS += [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     'TeacherApp',
 ]
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
